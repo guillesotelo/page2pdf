8a93c81a92843df1bf4a660697635eac911f4492
Guillermo Sotelo <guillermo.sotelo@volvocars.com>
2024-04-30 11:56:50 +0200

    generator

--- content.js
+++ content.js
@@ -1,5 +1,25 @@
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.action === "convertToPDF") {
-        console.log("Converting page to PDF...")
-    }
+    if (message.action === "convertToPDF") convertPageToPDF()
 })
+
+const convertPageToPDF = () => {
+    window.jsPDF = window.jspdf.jsPDF
+
+    console.log("Converting page to PDF...")
+
+    let svgElements = document.body.querySelectorAll('svg')
+    svgElements.forEach(function (item) {
+        item.setAttribute("width", item.getBoundingClientRect().width)
+        item.setAttribute("height", item.getBoundingClientRect().height)
+        item.style.width = null
+        item.style.height = null
+    })
+
+    const doc = new jsPDF()
+    doc.html(document.body, {
+        callback: function (doc) {
+            doc.save('webpage.pdf')
+        }
+    })
+}
