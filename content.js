a08d95bfc81ff934d30a6a9d12d678bf5de17670
Guillermo Sotelo <guillermo.sotelo@volvocars.com>
2024-04-30 14:45:11 +0200

    generation-without-imgs

--- content.js
+++ content.js
@@ -1,25 +1,61 @@
-
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.action === "convertToPDF") convertPageToPDF()
 })
 
-const convertPageToPDF = () => {
-    window.jsPDF = window.jspdf.jsPDF
-
-    console.log("Converting page to PDF...")
-
-    let svgElements = document.body.querySelectorAll('svg')
-    svgElements.forEach(function (item) {
-        item.setAttribute("width", item.getBoundingClientRect().width)
-        item.setAttribute("height", item.getBoundingClientRect().height)
-        item.style.width = null
-        item.style.height = null
-    })
-
-    const doc = new jsPDF()
-    doc.html(document.body, {
-        callback: function (doc) {
-            doc.save('webpage.pdf')
+const imageUrlToBase64 = async (url) => {
+    const data = await fetch(url)
+    const blob = await data.blob()
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.readAsDataURL(blob)
+        reader.onloadend = () => {
+            const base64data = reader.result
+            resolve(base64data)
         }
+        reader.onerror = reject
+    })
+}
+
+const replaceImageSourcesWithBase64 = () => {
+    const images = document.querySelectorAll('img')
+    Array.from(images).forEach(img => {
+        img.src = imageUrlToBase64(img.src)
+    })
+}
+
+const convertPageToPDF = () => {
+    console.log("Converting page to PDF...")
+    window.jsPDF = window.jspdf.jsPDF
+    // replaceImageSourcesWithBase64()
+
+    html2canvas(document.body,
+        {
+            // logging: true,
+            letterRendering: 1,
+            // allowTaint: true,
+            // useCORS: true
+        }
+    ).then(function (canvas) {
+        // Convert the canvas to a data URL
+        var imgData = canvas.toDataURL('image/png')
+        var imgWidth = 210
+        var pageHeight = 295
+        var imgHeight = canvas.height * imgWidth / canvas.width
+        var heightLeft = imgHeight
+        var doc = new jsPDF('p', 'mm')
+        var position = 10 // give some top padding to first page
+
+        doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight)
+        heightLeft -= pageHeight
+
+        while (heightLeft >= 0) {
+            position += heightLeft - imgHeight // top padding for other pages
+            doc.addPage()
+            doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight)
+            heightLeft -= pageHeight
+        }
+
+        doc.save("page.pdf")
+        chrome.runtime.sendMessage({ action: "processComplete" })
     })
 }
--- popup.js
+++ popup.js
@@ -10,8 +10,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
             chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, { action: "convertToPDF" })
-                setTimeout(() => window.close(), 2000)
             })
         })
     }
+
+    chrome.runtime.onMessage.addListener(function (message) {
+        if (message.action === "processComplete") {
+            window.close()
+        }
+    })
 })
