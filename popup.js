4de9a3adc693b6170523dda2f6242432abf5ca13
Guillermo Sotelo <guillermo.sotelo@volvocars.com>
2024-04-30 10:27:37 +0200

    btns

---
+++ popup.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const convertButton = document.getElementById("convertButton")
+    if (convertButton) {
+        convertButton.addEventListener("click", function () {
+            const loader = document.querySelector('.loader')
+            const loaderText = document.querySelector('.loaderText')
+            if (loader) loader.style.display = 'block'
+            if (loaderText) loaderText.style.display = 'block'
+            convertButton.style.display = 'none'
+
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { action: "convertToPDF" })
+                setTimeout(() => window.close(), 2000)
+            })
+        })
+    }
+})
