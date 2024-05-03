749018368449ba85116b8693d8954ed721a5e3c3
Guillermo Sotelo <guillermo.sotelo@volvocars.com>
2024-05-03 13:54:19 +0200

    image-converter

--- content.js
+++ content.js
@@ -21,15 +21,32 @@ const scrollDownUntilEnd = () => {
     scroll()
 }
 
-const imageUrlToBase64 = (img) => {
-    var canvas = document.createElement("canvas")
-    canvas.width = img.width
-    canvas.height = img.height
-    var ctx = canvas.getContext("2d")
-    ctx.drawImage(img, 0, 0)
-    var dataURL = canvas.toDataURL("image/png")
-    return dataURL
-}
+const imageUrlToBase64 = (imgSrc) => {
+    return new Promise((resolve, reject) => {
+        // Fetch the image data as a Blob
+        fetch(imgSrc, { mode: 'cors' })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.blob();
+            })
+            .then(blob => {
+                // Read the Blob as a data URL
+                const reader = new FileReader();
+                reader.onload = function() {
+                    resolve(reader.result);
+                };
+                reader.onerror = function() {
+                    resolve(imgSrc)
+                };
+                reader.readAsDataURL(blob);
+            })
+            .catch(error => {
+                resolve(imgSrc)
+            });
+    });
+};
 
 const replaceImageSourcesWithBase64 = () => {
     const images = document.querySelectorAll('img')
@@ -40,20 +57,46 @@ const replaceImageSourcesWithBase64 = () => {
     })
 }
 
-const convertPageToPDF = () => {
+const convertPageToPDF = async () => {
     console.log("Converting page to PDF...")
     const title = document.querySelector('title')
+    const { width } = document.body.getBoundingClientRect()
+
+    window.scrollTo(0, 0)
+    document.body.style.padding = '0 10px'
+
+    let imagenes = document.getElementsByTagName("img");
+    for (let i = 0; i < imagenes.length; i++) {
+        if(imagenes[i].src.includes('://')) {
+            const newSource = await imageUrlToBase64(imagenes[i].src)
+            console.log(newSource)
+            imagenes[i].src = newSource
+        }
+    }
+
+    const marginX = (window.innerWidth - width) / 2
+
+    console.log('width', width)
+    console.log('window.innerWidth', window.innerWidth)
     var options = {
-        margin: 0,
-        filename: `${title.textContent}.pdf`,
+        margin: [0, marginX],
+        filename: `${title ? title.textContent : 'page'}.pdf`,
         image: { type: 'jpeg', quality: 0.98 },
         html2canvas: {
-            // width: window.innerWidth,
+            width: width,
+            windowWidth: width,
+            // scale: 2,
             allowTaint: true,
-            useCORS: true
+            useCORS: true,
+            dpi: 300
         },
-        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+        // jsPDF: {
+        // margin: 5, 
+        // format: 'letter', 
+        // orientation: 'portrait'
+        // }
     }
-    html2pdf().set(options).from(document.body).save()
-    chrome.runtime.sendMessage({ action: "processComplete" })
+    html2pdf().set(options).from(document.body).save().then(() => {
+        chrome.runtime.sendMessage({ action: "processComplete" })
+    })
 }
